6a63129d5c1d805b71cac3d8ccb4c6a4af7db4fd
RomuloSousa1 <romullosousa@outlook.com.br>
2021-08-31 14:15:03 -0300

    Organizando o main.dart e criando o arquivo dashboard e contacts_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alura_projeto/screens/Dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankApp());
@@ -19,52 +20,4 @@ class BytebankApp extends StatelessWidget {
   }
 }
 
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset(
-              'images/bytebank_logo.png',
-              scale: 1.5,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              width: 130,
-              height: 100,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
